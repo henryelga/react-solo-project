c85b356298e990378ec0627efa6a221d86c16a7d
Elga J Henry <henryelga2004@gmail.com>
2023-01-18 22:15:14 +0000

    +

---
+++ Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Main(){
+    return(
+    <div className='main'>
+        <img src="image.png" alt="" />
+        <h3>Elga J Henry</h3>
+        <h5>Software Development Student</h5>
+        <small>elgajerushahenry.gatsbyjs.io</small>
+    </div>
+    )
+}
