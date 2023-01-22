91890b7da535eb6eb7a26fed111206965f7d2e38
Elga J Henry <henryelga2004@gmail.com>
2023-01-22 22:08:02 +0000

    +

--- js/Info.jsx
+++ js/Info.jsx
@@ -6,7 +6,7 @@ export default function Info(){
         <img src="image.png" alt="" />
         <h3>Elga J. Henry</h3>
         <h5>Software Development Student</h5>
-        <small>elgajerushahenry.gatsbyjs.io</small>
+        <small>Portfolio: elgajerushahenry.gatsbyjs.io</small>
         <div>
             <button>Email</button>
             <button>LinkedIn</button>
